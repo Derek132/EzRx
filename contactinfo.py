8bea299f860c2b47897f730ba82b319793bfba93
Kenshin <lekichnhuong@gmail.com>
2023-02-09 09:36:14 -0800

    Initial Commit

---
+++ contactinfo.py
@@ -0,0 +1,32 @@
+class ContactInfo:
+    def __int__(self, PatientName, Street, City, State, Zip, Phone, DOB, LastVisit, Email, RxCount, MMDD, HomeName, PatientID, CustomID, HipaaSigID, PatientNotes, CreatedDate,
+                Patcell, DeliveryMethod, AR_Account, Sex, DeceasedDate, Language, PatCellVerified):
+
+        self.PatientName = PatientName
+        self.Street = Street
+        self.City = City
+        self.State = State
+        self.Zip = Zip
+        self.Phone = Phone
+        self.DOB = DOB
+        self.LastVisit = LastVisit
+        self.Email = Email
+        self.RxCount = RxCount
+        self.MMDD = MMDD
+        self.HomeName = HomeName
+        self.PatientID = PatientID
+        self.CustomID = CustomID
+        self.HipaaSigID = HipaaSigID
+        self.PatientNotes = PatientNotes
+        self.CreatedDate = CreatedDate
+        self.Patcell = Patcell
+        self.DeliveryMethod = DeliveryMethod
+        self.AR_Account = AR_Account
+        self.Sex = Sex
+        self.DeceasedDate = DeceasedDate
+        self.Language = Language
+        self.PatCellVerified = PatCellVerified
+
+
+
+
